a53e60e309968b94cd9914050cf2c5139a679f5e
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2026-01-06 21:22:05 +0100

    add wpg setup script

---
+++ .dotfiles/setup-wpg.zsh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+cd "$HOME" || exit
+
+if command -v wpg-install.sh >/dev/null; then
+  echo "Installing wpgtk themes and icons"
+  wpg-install.sh -gi
+else
+  echo "ERROR: command wpg-install.sh not found"
+  echo "Please install wpgtk and run the command wpg-install -gi"
+fi
+
+# Some wallpapers: https://wallpaperaccess.com/minimalism-4k
+# To build your own theme also see:
+# https://github.com/deviantfero/wpgtk-colorschemes
+# https://github.com/deviantfero/wpgtk-templates
+# https://github.com/deviantfero/wpgtk/wiki/Templates
+
+download_wallpaper() {
+  local url="$1"
+  local wallpaper="$2"
+
+  local wallpaper_path="$HOME/.config/wallpapers"
+  mkdir -p "$wallpaper_path"
+
+  echo "Downloading wallpaper to $wallpaper_path/$wallpaper"
+  curl -L "$url" --output "$wallpaper_path/$wallpaper"
+
+  if command -v wpg >/dev/null; then
+    echo "Add wallpaper to wpg"
+    wpg -a "$wallpaper_path/$wallpaper"
+  else
+    echo "Run wpg to generate the config from the template files"
+  fi
+}
+
+download_wallpaper https://wallpaperaccess.com/full/2109997.jpg pyramid.jpg
+download_wallpaper https://wallpaperaccess.com/full/10750888.jpg period.jpg
+download_wallpaper https://wallpaperaccess.com/full/3205251.jpg space.jpg
+download_wallpaper https://wallpaperaccess.com/full/1582719.jpg panda.jpg
+
+
+ 
+if command -v wpg >/dev/null; then
+  echo "Set wallpaper"
+  wpg -a pyramid.jpg
+fi
+
+echo "Run lxappearance to set the GTK and icon theme"
